56a4190c5985f1bd1ea5967d3adcb685938e595e
Dawe000 <100613136+Dawe000@users.noreply.github.com>
2024-03-10 11:30:01 +0000

    made style changes

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -52,7 +52,9 @@ const Header = () => {
   }  
   
   
-  
+  const handleDisconnect = async () => {
+    
+  }
 
   const handleConnect = async () => {  
     let accounts = await window.ethereum?.request({  method: "eth_requestAccounts" })  
@@ -102,20 +104,7 @@ const Header = () => {
     >
       <div className="relative mx-auto max-w-c-1390 items-center justify-between px-4 md:px-8 xl:flex 2xl:px-0">
         <div className="flex w-full items-center justify-between xl:w-1/4">
-          <a href="/">
-            <Image
-              src="/images/logo/logo-dark.svg"
-              alt="logo"
-              width={119.03}
-              height={30}
-              className="hidden w-full dark:block" />
-            <Image
-              src="/images/logo/logo-light.svg"
-              alt="logo"
-              width={119.03}
-              height={30}
-              className="w-full dark:hidden" />
-          </a>
+          
 
           {/* <!-- Hamburger Toggle BTN --> */}
           <button
@@ -153,52 +142,7 @@ const Header = () => {
           className={`invisible h-0 w-full items-center justify-between xl:visible xl:flex xl:h-auto xl:w-full ${navigationOpen &&
             "navbar !visible mt-4 h-auto max-h-[400px] rounded-md bg-white p-7.5 shadow-solid-5 dark:bg-blacksection xl:h-auto xl:p-0 xl:shadow-none xl:dark:bg-transparent"}`}
         >
-          <nav>
-            <ul className="flex flex-col gap-5 xl:flex-row xl:items-center xl:gap-10">
-              {menuData.map((menuItem, key) => (
-                <li key={key} className={menuItem.submenu && "group relative"}>
-                  {menuItem.submenu ? (
-                    <>
-                      <button
-                        onClick={() => setDropdownToggler(!dropdownToggler)}
-                        className="flex cursor-pointer items-center justify-between gap-3 hover:text-primary"
-                      >
-                        {menuItem.title}
-                        <span>
-                          <svg
-                            className="h-3 w-3 cursor-pointer fill-waterloo group-hover:fill-primary"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512"
-                          >
-                            <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-                          </svg>
-                        </span>
-                      </button>
-
-                      <ul
-                        className={`dropdown ${dropdownToggler ? "flex" : ""}`}
-                      >
-                        {menuItem.submenu.map((item, key) => (
-                          <li key={key} className="hover:text-primary">
-                            <Link href={item.path || "#"}>{item.title}</Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </>
-                  ) : (
-                    <Link
-                      href={`${menuItem.path}`}
-                      className={pathUrl === menuItem.path
-                        ? "text-primary hover:text-primary"
-                        : "hover:text-primary"}
-                    >
-                      {menuItem.title}
-                    </Link>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </nav>
+          
 
           <div className="mt-7 flex items-center gap-6 xl:mt-0">
             <ThemeToggler />
@@ -208,9 +152,11 @@ const Header = () => {
             <div>Injected Provider {hasProvider ? 'DOES' : 'DOES NOT'} Exist</div>
       
       { wallet.accounts.length > 0 ?  
-        <div>Wallet Account: { wallet.accounts[0] }</div> : <button onClick={handleConnect}>Connect MetaMask</button>
+        <div>Wallet Account: { wallet.accounts[0] }</div> : <button onClick={handleConnect} >Connect MetaMask</button>
       }
-    
+      { wallet.accounts.length > 0 &&  
+      <button onClick={handleDisconnect}>Disconnect Metamask</button>
+      }
   
     
           </div>
